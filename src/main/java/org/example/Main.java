6d8965160821f23fe8d7444d499a4c651ed6bdcd
HellsTomato <148013078+HellsTomato@users.noreply.github.com>
2025-04-30 20:33:46 +0300

    Delete src/main/java/org/example directory

--- src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Order order = new Order(1, "Иван Иванов", 1500.0, 3, true);
-        OrderService service = new OrderService();
-        service.handle(order);
-    }
-}
